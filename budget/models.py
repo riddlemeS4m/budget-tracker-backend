46aafed9684d51eead9cd393ba24b35de8e2b256
Sam Riddle <sriddle@quanthub.com>
2026-02-22 14:17:11 -0600

    Add subcategory to backend
    - And update openapi schema

--- budget/models.py
+++ budget/models.py
@@ -60,6 +60,7 @@ class Transaction(models.Model):
     description = models.CharField(max_length=255, null=True, blank=True)
     description_2 = models.CharField(max_length=255, null=True, blank=True)
     category = models.CharField(max_length=255, null=True, blank=True)
+    subcategory = models.CharField(max_length=255, null=True, blank=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     raw_data = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
